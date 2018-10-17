6d0ee162755baf04e3ae78871e2a6ee60750af58
Julian Chang <julianjiachichang@hotmail.com>
2018-10-17 09:48:09 -0700

    Update team7.py
    We changed team name, strategy name, strategy description.
    We created a move function that returns collude if the other team colluded twice in a row.

--- team7.py
+++ team7.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team Seven !' # Only 10 chars displayed.
+strategy_name = 'if they nice, we nice'
+strategy_description = 'we will only be nice if they are nice two times in a row.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +17,16 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if my_history == '':
+        return 'c'
+    else: 
+        if their_history[-1] == 'b':
+            helper(my_score, their_score)
+        else:
+            if their_history[-2] == 'c' :
+                return 'c'
+            else:
+                'b'
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -27,6 +36,14 @@ def move(my_history, their_history, my_score, their_score):
     # Decide whether to return 'c' or 'b'.
     
     return 'c'
+
+
+def helper(my_score, their_score):
+    if int(their_score) - int(our_score) >= 600:
+        return 'b'
+    else:
+        return 'c'
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
